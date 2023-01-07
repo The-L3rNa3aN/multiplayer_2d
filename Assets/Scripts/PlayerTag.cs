c914a58fc6eb436b6e24e9a3559d5213fce02429
Veman Jadhav <vemancraft@gmail.com>
2023-01-07 11:55:12 +0530

    Update PlayerTag.cs

--- Assets/Scripts/PlayerTag.cs
+++ Assets/Scripts/PlayerTag.cs
@@ -6,16 +6,21 @@ using TMPro;
 
 public class PlayerTag : NetworkBehaviour
 {
-    public string playerName;
     public TextMeshPro nameplate;
+    private string playerName;
 
     private void Start()
     {
         if (!isLocalPlayer) return;
         playerName = PlayerPrefs.GetString("localPlayerName");
+    }
+
+    private void Update()
+    {
         CmdSetName(playerName);
         nameplate.text = playerName;
     }
+
     [Command] public void CmdSetName(string n) => RpcSetName(n);
     [ClientRpc] public void RpcSetName(string n) => playerName = n;
 }
